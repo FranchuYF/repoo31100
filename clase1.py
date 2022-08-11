a36c9f196cf36a786b5a1b18dc4f1732e6919187
Franco Rimmele <fran.rimmele03@gmail.com>
2022-08-10 22:54:11 -0300

    este commit modifique aprobado y clase

--- clase1.py
+++ clase1.py
@@ -1,9 +1,10 @@
 class Persona:
 
-    def __init__(self,nombre,apellido,edad):
+    def __init__(self,nombre,apellido,edad, dni):
         self.nombre=nombre
         self.apellido=apellido
         self.edad=edad
+        self.dni = dni
     
     def hablar(self):
         print(f"Hola mi nombre es {self.nombre} {self.apellido} y tengo {self.edad} anios")
